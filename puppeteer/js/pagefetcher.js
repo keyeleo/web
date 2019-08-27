25d4bff2a2ba235cf5bd0387b2e22805fb486fc8
vic <vic@hadrew.com>
2019-08-27 22:37:54 +0800

    fetch from sina;

--- puppeteer/js/pagefetcher.js
+++ puppeteer/js/pagefetcher.js
@@ -48,17 +48,42 @@ class PageFetcher{
     const page = await this.browser.newPage();
     await page.goto(url);
 
-    console.log('df='+this.dataFetcher+', ug='+this.urlGenerator);
-    this.dataFetcher.fetch();
-    this.urlGenerator.generate();
+    //page.evaluate 方法运行在页面内部，并返回Promise到外部
+    const bodyHandle = await page.$('body');
+    const result = await page.evaluate((bodyHandle) => {
+      let divList = [];
+      let stockList = [];
 
-    await page.evaluate(()=>{
-      console.log('df='+this.dataFetcher+', ug='+this.urlGenerator+', document='+document);
-      // dataFetcher.fetch(document);
-      // urlGenerator.generate(document);
-    });
+      //all div
+      let divHandles=bodyHandle.querySelectorAll('div');
+      for(let divHandle of divHandles){
+        let divObj={'id':divHandle.id, 'class':divHandle.class};
+        divList.push(divObj);
+      }
+
+      //涨幅榜
+      let cnltContentHandle=bodyHandle.querySelector('#cnlt_contents');
+      let tbodyHandle=cnltContentHandle.querySelector('tbody');
+      let trHandles=tbodyHandle.querySelectorAll('tr');
+      for(let trHandle of trHandles){
+        let stock=[];
+        let thHandles=trHandle.querySelectorAll('th');
+        stock.push(thHandles[0].textContent);
+        stock.push(thHandles[1].textContent);
+
+        let tdHandles=trHandle.querySelectorAll('td');
+        for(let td of tdHandles){
+          stock.push(td.textContent);
+        }
+        stockList.push(stock);
+      }
+
+      return {'div':divList, 'stocks':stockList};
+    }, bodyHandle);
+    await bodyHandle.dispose();
+    console.log(result);
+
     // await page.screenshot({path: imagePath});
-
     console.log('fetch page: '+url+', dataPath: '+dataPath+', imagePath: '+imagePath);
   }
 }
