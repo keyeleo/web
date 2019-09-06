96c6496031cb3feb64e008159a1c986e3a9022ab
vic <keyeleo@163.com>
2019-09-06 17:48:09 +0800

    process parallel;

--- puppeteer/js/fetch_financial.js
+++ puppeteer/js/fetch_financial.js
@@ -19,26 +19,24 @@ exports = module.exports = class FetchFinancialTrigger{
 		// this.processList(dd);
 		// return 'process 000876';
 
-		let sql='SELECT id FROM summary WHERE id<\'001000\' OR id>\'300000\' AND id<\'300100\' ORDER BY id';
+		let sql='SELECT id FROM summary WHERE id<\'001000\' OR id>\'300000\' AND id<\'300100\' OR id>\'600000\' AND id<\'604000\' ORDER BY id';
 		let res=await db.query('stocks',sql);
-		data={'sz': res.rows.length};
-		//async process list
-		this.processList(res);
 
-		sql='SELECT id FROM summary WHERE id>\'600000\' AND id<\'604000\' ORDER BY id';
-		res=await db.query('stocks',sql);
-		data.sh=res.rows.length;
-		//async process list
-		this.processList(res);
-
-		return data;
+		var ids=[];
+		const parallel=8;
+		const bundle=1+res.rows.length/parallel;
+		for(let i=0,ii=res.rows.length;i<ii;++i){
+			ids.push(res.rows[i].id);
+			if(ids.length>=bundle || i==ii-1){
+				//async process list
+				this.processList(ids);
+				ids=[];
+			}
+		}
+		return {'total': res.rows.length};
 	}
 
-	async processList(data){
-		let ids=[];
-		for(let row of data.rows){
-			ids.push(row.id);
-		}
+	async processList(ids){
 		for(let code of ids){
 			const url='http://quotes.money.163.com/f10/zycwzb_'+code+'.html';
 	        await PageFetcher.fetch(url);
